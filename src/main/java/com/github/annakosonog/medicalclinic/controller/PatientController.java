91fde40ab00103dd6f64768b41dac73570b87749
annaKosonog <anna.kosonog21@gmail.com>
2023-01-02 18:02:52 +0100

    + MED-06

--- src/main/java/com/github/annakosonog/medicalclinic/controller/PatientController.java
+++ src/main/java/com/github/annakosonog/medicalclinic/controller/PatientController.java
@@ -1,7 +1,9 @@
 package com.github.annakosonog.medicalclinic.controller;
 
-import com.github.annakosonog.medicalclinic.exception.PatientNotFoundException;
 import com.github.annakosonog.medicalclinic.model.Patient;
+import com.github.annakosonog.medicalclinic.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,75 +15,49 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+@RequiredArgsConstructor
 @RequestMapping("/patients")
 @RestController
 public class PatientController {
 
-    private List<Patient> patients = new ArrayList<>();
-
+    private final PatientService service;
 
     @GetMapping
     public ResponseEntity<List<Patient>> getAllPatients() {
-        return ResponseEntity.ok(patients);
+        return ResponseEntity.ok(service.getPatients());
     }
 
     @GetMapping("/{email}")
     public ResponseEntity<Patient> getPatient(@PathVariable String email) {
-        return getPatientByEmail(email)
-                .map(ResponseEntity::ok)
-                .orElseThrow(PatientNotFoundException::new);
+        return ResponseEntity.ok(service.getPatient(email));
     }
 
 
     @PostMapping
     public ResponseEntity<String> addPatient(@RequestBody Patient patient) {
-        patients.add(patient);
-        return ResponseEntity.ok("Patient was added successfully");
+        service.addPatient(patient);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{email}")
-    public ResponseEntity<String> deletePatient(@PathVariable("email") String email) {
-        getPatientByEmail(email)
-                .map(deletePatients -> ResponseEntity.ok(patients.remove(deletePatients)))
-                .orElseThrow(PatientNotFoundException::new);
-
+    public ResponseEntity<String> deletePatient(@PathVariable String email) {
+        service.deletePatient(email);
         return ResponseEntity.ok("Patient was deleted successfully");
     }
 
     @PutMapping("/{email}")
     public ResponseEntity<String> updatePatient(@PathVariable("email") String email, @RequestBody Patient patient) {
-        final Optional<Patient> patientDate = getPatientByEmail(email);
-        if (patientDate.isPresent()) {
-            Patient updatePatient = patientDate.get();
-            updatePatient.setEmail(patient.getEmail());
-            updatePatient.setPassword(patient.getPassword());
-            updatePatient.setIdCardNo(patient.getIdCardNo());
-            updatePatient.setFirstName(patient.getFirstName());
-            updatePatient.setLastName(patient.getLastName());
-            updatePatient.setNumberPhone(patient.getNumberPhone());
-            updatePatient.setBirthday(patient.getBirthday());
-            patients.add(updatePatient);
-            return ResponseEntity.ok("Patient was update successfully");
-        } else {
-            return ResponseEntity.ok("Patient  update failed");
-        }
+        service.updatePatient(patient);
+        return ResponseEntity.ok("Patient was update successfully");
     }
 
     @PatchMapping("/{email}")
     public ResponseEntity<String> editPatientPassword(@PathVariable String email, @RequestBody String password) {
-        Patient patient = getPatientByEmail(email).orElseThrow(PatientNotFoundException::new);
-        patient.setPassword(password);
+        service.updatePasswordPatient(email, password);
         return ResponseEntity.ok("Password changed successfully");
     }
 
 
-    private Optional<Patient> getPatientByEmail(String email) {
-        return patients.stream()
-                .filter(patient -> patient.getEmail().equals(email))
-                .findFirst();
-    }
 }
