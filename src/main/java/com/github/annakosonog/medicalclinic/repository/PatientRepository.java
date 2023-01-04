32569287e3b82c240fe203a8858eb739751b25b2
annaKosonog <anna.kosonog21@gmail.com>
2023-01-04 16:42:50 +0100

    + MED-07

---
+++ src/main/java/com/github/annakosonog/medicalclinic/repository/PatientRepository.java
@@ -0,0 +1,21 @@
+package com.github.annakosonog.medicalclinic.repository;
+
+import com.github.annakosonog.medicalclinic.model.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PatientRepository  {
+
+    List<Patient> findAll();
+
+    Optional<Patient> findByEmail(String email);
+
+    void save(Patient patient);
+
+    void delete(String email);
+
+    void update(String email, Patient patient);
+
+    void updatePassword(String email, String password);
+}
--- src/main/java/com/github/annakosonog/medicalclinic/service/PatientService.java
+++ src/main/java/com/github/annakosonog/medicalclinic/service/PatientService.java
@@ -3,66 +3,52 @@ package com.github.annakosonog.medicalclinic.service;
 import com.github.annakosonog.medicalclinic.exception.PatientAlreadyExistsException;
 import com.github.annakosonog.medicalclinic.exception.PatientNotFoundException;
 import com.github.annakosonog.medicalclinic.model.Patient;
+import com.github.annakosonog.medicalclinic.repository.PatientRepositoryImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class PatientService {
 
-    private List<Patient> patients = new ArrayList<>();
+
+    private final PatientRepositoryImpl patientRepository;
 
 
     public List<Patient> getPatients() {
-        return patients;
+       return patientRepository.findAll();
     }
 
-
     public Patient getPatient(String email) {
-        return getPatientByEmail(email).orElseThrow(PatientNotFoundException::new);
+        return patientRepository.findByEmail(email).orElseThrow(PatientNotFoundException::new);
     }
 
     public void addPatient(Patient patient) {
-        Optional<Patient> existingPatient = patients.stream()
-                .filter(patient1 -> patient1.getEmail().equals(patient.getEmail()))
-                .findFirst();
+        Optional<Patient> existingPatient =patientRepository.findByEmail(patient.getEmail());
         if (existingPatient.isPresent()) {
             throw new PatientAlreadyExistsException();
         }
         if (patient.getEmail() == null) {
             throw new IllegalArgumentException("Invalid patient data");
         }
-        patients.add(patient);
     }
 
-    public void deletePatient(String email){
-        final Patient patient = getPatientByEmail(email).orElseThrow(PatientNotFoundException::new);
-       patients.remove(patient);
+    public void deletePatient(String email) {
+        patientRepository.delete(email);
     }
 
-    public void  updatePatient(Patient patient, String email) {
-        Patient patientData = getPatientByEmail(email)
+    public void updatePatient(Patient patient, String email) {
+        Patient patientData = patientRepository.findByEmail(email)
                 .orElseThrow((PatientNotFoundException::new));
-
-        patientData.setEmail(patient.getEmail());
-        patientData.setPassword(patient.getPassword());
-        patientData.setIdCardNo(patient.getIdCardNo());
-        patientData.setFirstName(patient.getFirstName());
-        patientData.setLastName(patient.getLastName());
-        patientData.setNumberPhone(patient.getNumberPhone());
-        patientData.setBirthday(patient.getBirthday());
-        }
-
-    public void updatePasswordPatient(String email, String password){
-        Patient patients = getPatientByEmail(email).orElseThrow(PatientNotFoundException::new);
-        patients.setPassword(password);
+        patientRepository.update(email, patient);
     }
 
-    private Optional<Patient> getPatientByEmail(String email) {
-        return patients.stream()
-                .filter(patient -> patient.getEmail().equals(email))
-                .findFirst();
+    public void updatePasswordPatient(String email, String password) {
+        patientRepository.findByEmail(email)
+                .orElseThrow(PatientNotFoundException::new);
+        patientRepository.updatePassword(email, password);
     }
 }
